862952a4b4f541159bbed6a23fd299f78e49996d
Paul Osman <paul@honeycomb.io>
2020-11-20 13:39:22 -0600

    Remove unnecessary panic
    Not being able to subscribe to logs API events is not great, but not
    critical either - allow the extension to continue to run and not block
    invocations.

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func main() {
 
 	subRes, err := logsClient.Subscribe(ctx, extensionClient.ExtensionID, logTypes)
 	if err != nil {
-		log.Panic("Could not subscribe to events", err)
+		log.Warn("Could not subscribe to events", err)
 	}
 	log.Debug("Response from subscribe: ", subRes)
 
