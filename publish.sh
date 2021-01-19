6a49162edc7f7adbc425e3afd05070a3adaf8daf
Paul Osman <paul@honeycomb.io>
2021-01-19 16:05:14 -0600

    Limit publishing to tag pushes

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ -z "${CIRCLE_TAG}" ]]; then
+if [[ "${CIRCLE_TAG}" == *dev ]]; then
     EXTENSION_NAME="honeycomb-lambda-extension-dev"
 else
     EXTENSION_NAME="honeycomb-lambda-extension"
 fi
 
-REGIONS=(eu-north1 ap-south-1 eu-west-3 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1
+REGIONS=(eu-north-1 ap-south-1 eu-west-3 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1
          sa-east-1 ca-central-1 ap-southeast-1 ap-southeast-2 eu-central-1 us-east-1
          us-east-2 us-west-1 us-west-2)
 
