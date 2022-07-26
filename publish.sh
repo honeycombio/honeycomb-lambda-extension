7e3a911f37ea7a80aaed1401bcd916199eaf780c
Purvi Kanal <purvikanal@honeycomb.io>
2022-07-26 13:55:13 -0400

    add support for more aws regions

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ else
     EXTENSION_NAME="honeycomb-lambda-extension"
 fi
 
-REGIONS_NO_ARCH=(eu-north-1 us-west-1 eu-west-3 ap-northeast-2 sa-east-1 ca-central-1)
+REGIONS_NO_ARCH=(eu-north-1 us-west-1 eu-west-3 ap-northeast-2 sa-east-1 ca-central-1 af-south-1 ap-east-1 eu-south-1 me-south-1)
 REGIONS_WITH_ARCH=(ap-south-1 eu-west-2 us-east-1 eu-west-1 ap-northeast-1 ap-southeast-1
                    ap-southeast-2 eu-central-1 us-east-2 us-west-2)
 
