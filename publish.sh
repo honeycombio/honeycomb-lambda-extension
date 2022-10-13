ba3cc87691aa96942eb4f9ed6d143d161ddfed7e
Robb Kidd <robbkidd@honeycomb.io>
2022-10-12 20:08:35 -0400

    update list of regions that support multi-arch
    The combination of all of the regions we've been publishing to matches
    the list of regions that support both arm64 and x86_64 at the AWS Lambda
    pricing page as of 2022/10/12. Moving existing regions to the WITH_ARCH
    list.
    
    NO_ARCH is currently empty, but one new-to-us region is online and
    currently only supports x86_64. We can add it to NO_ARCH when we've
    granted the publishing credentials permission to publish layers there.

--- publish.sh
+++ publish.sh
@@ -21,10 +21,37 @@ else
     EXTENSION_NAME="honeycomb-lambda-extension"
 fi
 
-REGIONS_NO_ARCH=(eu-north-1 us-west-1 eu-west-3 ap-northeast-2 sa-east-1 ca-central-1 af-south-1
-                ap-east-1 eu-south-1 me-south-1 ap-southeast-3 ap-northeast-3)
-REGIONS_WITH_ARCH=(ap-south-1 eu-west-2 us-east-1 eu-west-1 ap-northeast-1 ap-southeast-1
-                   ap-southeast-2 eu-central-1 us-east-2 us-west-2)
+# Region list update from AWS Lambda pricing page as of 2022/10/12
+#
+# regions with x86_64 only support
+# REGIONS_NO_ARCH=(me-central-1) # listed on pricing page, but we need grant our publishing credentials permission to publish there first
+REGIONS_NO_ARCH=()
+# Regions with x86_64 & arm64
+REGIONS_WITH_ARCH=(
+    af-south-1
+    ap-east-1
+    ap-northeast-1
+    ap-northeast-2
+    ap-northeast-3
+    ap-south-1
+    ap-southeast-1
+    ap-southeast-2
+    ap-southeast-3
+    ca-central-1
+    eu-central-1
+    eu-north-1
+    eu-south-1
+    eu-west-1
+    eu-west-2
+    eu-west-3
+    me-south-1
+    sa-east-1
+    us-east-1
+    us-east-2
+    us-west-1
+    us-west-2
+)
+
 
 results_dir="publishing"
 mkdir -p ${results_dir}
